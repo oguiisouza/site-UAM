088a176a4f5a7e3c082bbb0af1469fbf113f50c1
unknown <souza.guisc@gmail.com>
2022-08-13 02:23:06 -0300

    Terceiro commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     $dbHost = 'localhost';
     $dbUsername = 'root';
-    $dbPassword = '18045370';
+    $dbPassword = '';
     $dbName = 'form';
     
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
